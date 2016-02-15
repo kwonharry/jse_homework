43b9209bf23e0cb4b8836c4b69b98795fa8636b1
user <user@user-PC>
2016-02-15 18:31:13 +0900

    d

--- jse-homework/src/homework/InputGugudan.java
+++ jse-homework/src/homework/InputGugudan.java
@@ -1,5 +1,7 @@
 package homework;
 
+import java.util.Scanner;
+
 public class InputGugudan {
 /*
  * 단수를 입력받아서 구구단을 출력함
@@ -9,4 +11,22 @@ public class InputGugudan {
  * for loop
  * 
  * */
+	
+	public static void main(String[] args) {
+		
+		int num=0,i=0,result=0;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("몇 단을 하실껀가요?");
+		num=scanner.nextInt();
+		if(num<=0){
+		System.out.println("잘못입력하셨습니다.");
+		}
+		else{
+	
+		for(i=1;i<10;i++){
+			result=num*i;
+			System.out.println(num+"*"+i+"="+result);
+		}
+	}
+		}
 }
--- jse-homework/src/homework/MonthEndDay.java
+++ jse-homework/src/homework/MonthEndDay.java
@@ -1,12 +1,44 @@
 package homework;
 
+import java.util.Scanner;
+
 public class MonthEndDay {
-/*
- * 월을 입력하면 해당 월의 끝날이 몇일인지 출력하는 프로그램
- 단, 2월은 29일로 입력
- 주의점: 입력값이 1~12를 넘어서면 잘못입력
- sw
- */
-	
-	
+	/*
+	 * 월을 입력하면 해당 월의 끝날이 몇일인지 출력하는 프로그램 단, 2월은 29일로 입력 주의점: 입력값이 1~12를 넘어서면 잘못입력
+	 * sw
+	 */
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("몇 월달입니까?");
+		int month=scanner.nextInt();
+		
+		int end=30;
+		switch (month) {
+		case 1:
+		case 3:
+		case 5:
+		case 7:
+		case 8:
+		case 10:
+		case 12:
+			end=31;
+			break;
+		case 4:
+		case 6:
+		case 9:
+		case 11:
+			end=30;
+			break;
+		case 2:
+			end=29;
+			break;
+		default:
+			System.out.println("잘못입력하셨습니다.");
+			break;
+		}
+		System.out.println("끝의 날은 "+end+"입니다.");
+
+	}
+
 }
